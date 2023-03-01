13065645de485226ddd8ff4f99b493a01cc5f7a1
codehub-jp <codehub3110@gmail.com>
2023-03-01 15:41:23 +0900

    initial commit

---
+++ carry/src/main/java/com/carry/carry/CarryApplication.java
@@ -0,0 +1,13 @@
+package com.carry.carry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryApplication.class, args);
+	}
+
+}
---
+++ carry/src/test/java/com/carry/carry/CarryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carry.carry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
